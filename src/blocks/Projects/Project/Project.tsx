406d0faf80b9adec072b2ae29227434b1a7ac549
Aleksandr Belozerov <allbel900@gmail.com>
2023-02-13 18:04:57 +0300

    project block

--- src/blocks/Projects/Project/Project.tsx
+++ src/blocks/Projects/Project/Project.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import s from './Project.module.scss'
+import plus from '../../../assets/image/plus.png';
 
 type JobPropsType = {
     title: string
     desc: string
     style: any
+    href: string
 }
 
 const Project = (props: JobPropsType) => {
     return (
         <div className={s.job}>
-            <div className={s.imgBox} style={props.style}>
-                <div className={s.look}>Смотреть</div>
-            </div>
+            <a href={props.href} className={s.imgBox} style={props.style} target={'_blank'}>
+                <span className={s.look}>
+                    <img src={plus} alt={'Plus'} />
+                </span>
+            </a>
             <div className={s.text}>
                 <h3 className={s.title}>{props.title}</h3>
                 <div className={s.desc}>{props.desc}</div>
--- src/blocks/Projects/Projects.tsx
+++ src/blocks/Projects/Projects.tsx
@@ -4,6 +4,7 @@ import smain from "../../common/styles/container.module.scss";
 import Project from "./Project/Project";
 import Title from "../../common/components/Title/Title";
 import socialImage from "../../assets/image/social_network.jpeg";
+import cardsImage from "../../assets/image/cards.jpg";
 import todoImage from "../../assets/image/todolist.jpeg";
 // @ts-ignore
 import Fade from 'react-reveal/Fade';
@@ -12,6 +13,11 @@ const Projects = () => {
     const social = {
         color: 'blue',
         backgroundImage: `url(${socialImage})`,
+    };
+
+    const cards = {
+        color: 'blue',
+        backgroundImage: `url(${cardsImage})`,
     };
 
     const todolist = {
@@ -27,12 +33,12 @@ const Projects = () => {
                 </Fade>
                 <Fade bottom>
                     <div className={s.projects}>
-                        <Project style={social} title={'Learning by cards'}
-                                 desc={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi esse eveniet hic, id labore neque quisquam tempore totam! Accusantium aliquid aperiam aspernatur beatae minima minus molestias nostrum obcaecati omnis possimus.'}/>
-                        <Project style={todolist} title={'Todo list'}
-                                 desc={'Animi esse eveniet hic, id labore neque quisquam tempore totam! Accusantium aliquid aperiam aspernatur beatae minima minus molestias nostrum obcaecati omnis possimus.'}/>
-                        <Project style={social} title={'Social network'}
-                                 desc={'Animi esse eveniet hic, id labore neque quisquam tempore totam! Accusantium aliquid aperiam aspernatur beatae minima minus molestias nostrum obcaecati omnis possimus.'}/>
+                        <Project style={cards} title={'Learning by cards'} href={'https://friday-project.vercel.app/'}
+                                 desc={'The card method is well studied, and its effectiveness in teaching has been proven by research and the experience of many users.'}/>
+                        <Project style={todolist} title={'Todo list'} href={'https://allbel.github.io/todolist/'}
+                                 desc={'Keeping a to-do list of things to do over a period of time, such as a day or a week, is one of the most powerful ways to increase personal effectiveness.'}/>
+                        <Project style={social} title={'Social network'} href={'https://allbel.github.io/Social-network/'}
+                                 desc={'A platform that is used for communication, dating, creating social relationships between people who have similar interests.'}/>
                     </div>
                 </Fade>
             </div>
